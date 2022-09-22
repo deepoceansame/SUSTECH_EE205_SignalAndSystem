6ba9130efba9d18526066ad7f5cf0af4c82815fd
txy <3088617041@qq.com>
2022-09-22 19:30:51 +0800

    first commit

---
+++ lab1/liveLab.m
@@ -0,0 +1,18 @@
+A = [1 2 3;4 5 6;7 8 9];
+B = [3 5 2; 5 2 8; 3 6 9];
+ans1 = A^2;
+ans2 = A/B;
+ans3 = 2*A;
+ans4 = A + 1j*B;
+ans5 = ans4.';
+disp('ans1');
+disp(ans1);
+disp('ans2');
+disp(ans2);
+disp('ans3');
+disp(ans3);
+disp('ans4');
+disp(ans4);
+disp('ans5');
+disp(ans5);
+
