058f58019fb1e71ede1d4fca3bb35e90f1d3c375
deepoceansame <11911817@mail.sustech.edu.cn>
2022-11-23 23:08:39 +0800

    lab5_report

--- lab5/lab5q3.m
+++ lab5/lab5q3.m
@@ -14,7 +14,7 @@ for i = 1:length(cutoff)
     x_f = filter(b, a, x);
     hold on;
     plot(t, x, t, x_f);
-    legend('original audio', 'envelope')
+    legennd('original audio', 'envelope')
     xlabel('t');
     title(sprintf('order 2 cutoff %dHz', cutoff(i)))
 end
@@ -26,20 +26,33 @@ for i = 1:length(order)
     [b, a] = butter(order(i), 200/(fs/2));
     x_f = filter(b, a, x);
     plot(t, x, t, x_f);
-    legend('orginal audio', 'envelope');
+    legennd('orginal audio', 'envelope');
     title(sprintf('Order: %d, Cutoff freq: 200Hz',order(i)));
 end
 %% tes
 
 order = 1:2:10;
 figure; hold('on');
-lege = repmat("", 1, length(order));
+legen = repmat("", 1, length(order));
 for i = 1:length(order)
     [b, a] = butter(order(i), 0.5);
     [h, w] = freqz(b, a);
     plot(w, abs(h));
-    lege(i) = sprintf('order=%d',order(i));
+    legen(i) = sprintf('order=%d',order(i));
 end
-legend(lege); xlabel('\omega'); xlabel('Amplitude');
+legend(legen); xlabel('\omega'); xlabel('Amplitude');
+grid('on'); axis([0 pi 0 1]);
+set(gca,'XTick', 0:pi/4:pi, 'XTickLabel',{'0','\pi/4','\pi/2','3\pi/4','\pi'});
+
+cutoff = 100:200:700;
+figure; hold('on');
+legen = repmat("", 1, length(cutoff));
+for i = 1:length(cutoff)
+    [b, a] = butter(2, cutoff(i)/(fs/2));
+    [h, w] = freqz(b, a);
+    plot(w, abs(h));
+    legen(i) = sprintf('cutoff=%d',cutoff(i));
+end
+legend(legen); xlabel('\omega'); xlabel('Amplitude');
 grid('on'); axis([0 pi 0 1]);
 set(gca,'XTick', 0:pi/4:pi, 'XTickLabel',{'0','\pi/4','\pi/2','3\pi/4','\pi'});
